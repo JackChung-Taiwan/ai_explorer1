fcb9a91bf9116cd08c283c8184da0969438c7c3f
JackChung-Taiwan <58715515+JackChung-Taiwan@users.noreply.github.com>
2026-06-20 17:34:52 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,13 @@
+player.onChat("1", function () {
+    UndergroundBase.buildArtificialCanal(
+    80,
+    11,
+    3,
+    RotationAngle.D0,
+    0,
+    0,
+    1,
+    STONE_BRICKS,
+    STONE
+    )
+})
